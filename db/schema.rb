387b223540f7a0f16471024c3fa21b7f2161d6ff
Oscar Tellez <oscartzgz@gmail.com>
2017-04-09 00:32:59 -0500

    admin attrib was added to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409013327) do
+ActiveRecord::Schema.define(version: 20170409052900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +34,9 @@ ActiveRecord::Schema.define(version: 20170409013327) do
     t.datetime "oauth_expires_at"
     t.string   "email"
     t.string   "image_url"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.boolean  "admin",            default: false
   end
 
 end
