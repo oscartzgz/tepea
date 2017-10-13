e4a3810e8261b6f202f9bc6f988f6150ac18d1ae
Oscar Tellez <oscartzgz@gmail.com>
2017-10-13 16:24:56 -0500

    Rails app was updated from 5.0.2 to 5.1.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'auth/facebook/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'sessions#destroy', as: 'signout'
-
-  root 'news#index'
-  resources :news
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
