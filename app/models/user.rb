f89af8b3108820a6307a7d237029e6e2d5da0b30
Oscar Tellez <oscartzgz@gmail.com>
2017-10-16 18:43:38 -0500

    Users cant register between devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,# :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   def self.find_or_create_from_auth_hash(auth_hash)
     user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create
