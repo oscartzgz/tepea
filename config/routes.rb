ca5653598bc3c1e6352fe635852ca0e17956a2dc
Oscar Tellez <oscartzgz@gmail.com>
2017-10-16 23:13:02 -0500

    Changed name routes for news

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/index'
   root 'welcome#index'
-  resources :news
+  resources :news, path: 'noticias'
+  get 'noticias', to: 'news#index'
   devise_for :users
   resources :users
 end
