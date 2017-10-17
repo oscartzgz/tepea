562dac906a4b7166fd92e8b4bf533a0ff0f95d60
Oscar Tellez <oscartzgz@gmail.com>
2017-10-16 23:14:09 -0500

    Deleted obsolete code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
-
-  def user_logued_in
-    unless current_user
-        flash[:before_login] = 'Para realizar esta acción, inicia sesion con facebook'
-        redirect_back fallback_location: root_path
-    end
-  end
 end
