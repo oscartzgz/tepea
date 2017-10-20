adba690a078025196b9b2f5e7da95db140252464
Oscar Tellez <oscartzgz@gmail.com>
2017-10-19 23:53:17 -0500

    Filters show events, set permisions, show all events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,11 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
 
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all.order(date: :asc)
+    @events = Event.where('date >= ?', Date.today).order(date: :asc)
   end
 
   # GET /events/1
