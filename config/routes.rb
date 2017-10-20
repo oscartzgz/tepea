60f483333ea6273bf781ffb8594cbacb24f67497
Oscar Tellez <oscartzgz@gmail.com>
2017-10-19 23:56:58 -0500

    Setting spanish name in event routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :events
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'welcome/index'
-  root 'welcome#index'
+  resources :events, path: 'eventos'
   resources :news, path: 'noticias'
-  get 'noticias', to: 'news#index'
   devise_for :users
   resources :users
+  get 'welcome/index'
+  root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
