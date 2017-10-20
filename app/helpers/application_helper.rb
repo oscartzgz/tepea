f3b97216964a2772715c73b80ce6b08bb94494e2
Oscar Tellez <oscartzgz@gmail.com>
2017-10-19 23:37:45 -0500

    Styles to show individual events and other fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def left_days(date)
+    today = Date.today
+
+    days = date - today
+    days = days.to_i
+    
+    if days == 0
+      'HOY'
+    elsif days == 1
+      "Falta #{days} día"
+    elsif days == -1
+      "Fue hace #{ (days * -1) } día"
+    elsif days > 1
+      "Faltan #{days} dias"
+    elsif days < 1
+      "Fue hace #{ (days * -1) } dias"
+    end
+  end
 end
