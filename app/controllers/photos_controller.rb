949a1518fee395ac40a570c8805e37c574b57d2d
Oscar Tellez <oscartzgz@gmail.com>
2017-10-22 12:19:47 -0500

    Complete styles and sctructure for photos section

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   # GET /photos
   # GET /photos.json
