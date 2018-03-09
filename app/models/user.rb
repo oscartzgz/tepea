242b068af01e76b5de0a11cac3c212603b4dbd79
Oscar Tellez <oscartzgz@gmail.com>
2018-03-08 19:06:07 -0600

    Set max length name for user by omniauth create

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
         user.provider = auth.provider
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
-        user.name = auth.info.name  # assuming the user model has a name
+        user.name = auth.info.name[0..NAME_MAX_LENGTH]  # assuming the user model has a name
         user.image = auth.info.image # assuming the user model has an image
         # If you are using confirmable and the provider(s) you use validate emails, 
         # uncomment the line below to skip the confirmation emails.
