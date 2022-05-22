c69eaac2ca8421f919a193ed4d8344198183aa7a
Oscar Tellez <oscartzgz@gmail.com>
2022-05-22 17:09:18 -0500

    Add omniauth with google and setup es locale

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   #        :recoverable, :rememberable, :validatable
 
   devise :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_google(email:, full_name:, uid:, avatar_url:)
+    find_or_create_by(email: email) do |user|
+      user.uid = uid,
+      user.full_name = full_name
+      user.avatar_url = avatar_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :users
   mount Sidekiq::Web => '/sidekiq'
-
   root "news#index"
-  get 'news/index'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    # get 'users/sign_in', to: 'users/sessions#new', as: :new_user_session
+    get 'users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+  end
+
 end
