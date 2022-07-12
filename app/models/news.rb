41bd963db639d203124cfb5e4711c0ba5a6d0875
Oscar Tellez <oscartzgz@gmail.com>
2022-07-12 00:28:01 -0500

    Administration for publish and unpublish news

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,6 +2,8 @@ class News < ApplicationRecord
   belongs_to :municipality
   has_one_attached :image
 
+  enum status: { unpublished: 0, published: 1 }
+
   validates_presence_of :title
 
   enum status: { unpublished: 0, published: 1 }
